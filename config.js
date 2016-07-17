e32b6ed5c8ef35c2037ef7d2a355a677e67c9391
Nathaniel Knopf <nathanielknopf@gmail.com>
2016-07-17 16:26:44 -0700

    add new causal paths

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var configs = {
 	"play_time": 60,
-	"use_db": true,
+	"use_db": false,
 	"http_port": 8080,
 	"https_port": 4343,
 	"private_key": "sslcert/server.key",
@@ -9,11 +9,4 @@ var configs = {
 	"mysql_user": "root",
 	"mysql_password": "cocolab",
 	"mysql_database": "game",
-	"big_rocks_scale": 1.2,
-	"big_rocks_prob_a": 0.9,
-	"big_rocks_prob_b": 0.5,
-	"small_rocks_scale": 0.6,
-	"small_rocks_prob_a": 0.1,
-	"small_rocks_prob_b": 0.5,
-	"small_rock_ratio": 0.5 //ratio of rocks that will spawn as small rocks
 }
